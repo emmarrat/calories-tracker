75efa57bd5023e9217f473e18b14b20597ec3d7f
Emir Marat <emmarrat@gmail.com>
2023-03-05 13:58:58 +0600

    feat: add meal card

---
+++ src/components/MealCard/MealCard.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import {MealType} from "../../types";
+
+interface Props {
+  meal: MealType;
+}
+
+const MealCard: React.FC<Props> = ({meal}) => {
+
+  return (
+    <div className="card border-0 mb-3 col-10 col-md-9 shadow-lg p-3 bg-body rounded">
+      <div className="card-body d-flex flex-column flex-md-row justify-content-between align-items-center">
+        <div className="col-12 col-md-6">
+          <div className="d-flex flex-column flex-sm-row flex-md-column align-items-center align-items-md-start justify-content-between mb-3">
+            <h5 className="card-title mb-0">{meal.time}</h5>
+            <p className="card-text mb-0">{meal.descr}</p>
+            <p className="card-text mb-0">{meal.date}</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6">
+          <div className="d-flex flex-column flex-md-row justify-content-between align-items-center">
+            <p className="card-text mb-3 m-md-0">{meal.calories} kcal</p>
+            <div className="d-flex flex-md-column align-items-center align-items-md-end justify-content-between w-50">
+              <Link to={"/edit-meal/" + meal.id} className="btn btn-warning text-light d-block mb-md-1 w-75 me-2 me-md-0">Edit</Link>
+              <button className="btn btn-danger d-block w-75">Delete</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MealCard;
--- src/types.d.ts
+++ src/types.d.ts
@@ -8,3 +8,11 @@ export interface MealTypeApi {
 export interface MealOnClientSide extends MealTypeApi {
   calories: string
 }
+
+export interface MealsListApi {
+  [id: string]: MealTypeApi;
+}
+
+export interface MealType extends MealTypeApi {
+  id: string;
+}
