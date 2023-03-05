391d0f770c37d79a69c4e5ad3f2065d17d4c1e03
Emir Marat <emmarrat@gmail.com>
2023-03-05 14:30:05 +0600

    feat: add fetching of all meals and displaying them in cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,52 @@
-import React from 'react';
-import {Route, Routes} from "react-router-dom";
+import React, {useCallback, useEffect, useState} from 'react';
+import {Route, Routes, useLocation} from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import AddMeal from "./containers/AddMeal/AddMeal";
 import Home from "./containers/Home";
+import {MealsListApi, MealType} from "./types";
+import axiosApi from "./axiosApi";
 
 
 function App() {
+  const location = useLocation();
 
+  const [meals, setMeals] = useState<MealType[]>([]);
+  const [loading, setLoading] = useState(false);
+
+
+  const fetchMeals = useCallback(async () => {
+    try {
+      setLoading(true);
+      const mealsResponse = await axiosApi.get<MealsListApi | null>('/meals.json');
+      const meals = mealsResponse.data;
+      let newMeals: MealType[] = [];
+
+      if (meals) {
+        newMeals = Object.keys(meals).map(id => {
+          const meal = meals[id];
+          return {
+            ...meal,
+            id,
+          }
+        });
+        const sortedMeals = newMeals.sort(function (a, b) {
+          return Date.parse(b.date) - Date.parse(a.date);
+        });
+        setMeals(sortedMeals);
+      } else {
+        setMeals(newMeals);
+      }
+
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      void fetchMeals();
+    }
+  }, [fetchMeals, location]);
 
 
   return (
@@ -16,7 +56,11 @@ function App() {
       </header>
       <main className="container pt-5">
         <Routes>
-          <Route path="/" element={(<Home/>)}/>
+          <Route path="/" element={(
+            <Home
+              meals={meals}
+              loading={loading}
+          />)}/>
           <Route path="/add-new-meal" element={<AddMeal/>}/>
           <Route path="*" element={<h2 className="text-center mt-5">Page not found!</h2>}/>
         </Routes>
--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,14 +1,45 @@
 import React from 'react';
 import AddMealButton from "../components/AddMealButton/AddMealButton";
+import {MealType} from "../types";
+import MealCard from "../components/MealCard/MealCard";
+import Spinner from "../components/Spinner/Spinner";
 
-const Home = () => {
-  return (
-    <div className="d-md-flex justify-content-md-between">
-      <div>
-        <h3>Total calories for today: </h3>
-      </div>
-      <AddMealButton/>
+interface Props {
+  meals: MealType[];
+  loading: boolean;
+}
+
+const Home: React.FC<Props> = ({meals, loading}) => {
+
+  let content = (
+    <div className="mt-5 d-flex flex-column align-items-center">
+      {meals.map(meal => (
+        <MealCard meal={meal} key={meal.id}/>
+      ))}
     </div>
+  );
+
+  if (meals.length === 0) {
+    content = (
+      <>
+        <h3 className="text-center mt-5">List of meals is empty...</h3>
+      </>
+    );
+  }
+  return (
+    <>
+      {loading ? <Spinner/> : (
+        <>
+          <div className="d-md-flex justify-content-md-between">
+            <div>
+              <h3>Total calories for today:</h3>
+            </div>
+            <AddMealButton/>
+          </div>
+          {content}
+        </>
+      )}
+    </>
   );
 };
 
