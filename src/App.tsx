8a51710174a97d30ded334ed5531487d57027d48
Emir Marat <emmarrat@gmail.com>
2023-03-05 13:40:47 +0600

    feat: installed react-router, axios, bootstrap
    update: removed not necessary dependencies and files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from "react-router-dom";
+
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
       </header>
-    </div>
+      <main className="container pt-5">
+        <Routes>
+          <Route path="*" element={<h2 className="text-center mt-5">Page not found</h2>}/>
+        </Routes>
+      </main>
+    </>
   );
 }
 
